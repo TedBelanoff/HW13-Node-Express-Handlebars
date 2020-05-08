841cbbfd9a35c28ebb13773f651e32eeba9477be
APPLE BANANA <tedbelanoff@gmail.com>
2020-05-08 18:04:25 -0400

    HW13

--- config/connection.js
+++ config/connection.js
@@ -23,23 +23,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); 
-                                              
-
-  connection.connect(function(err) {              
-    if(err) {                                     
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                        
-    } else {                                      
-       throw err;                                
-    };
-});
-handleDisconnect()
-
 // ORM Connection
 module.exports = connection;
