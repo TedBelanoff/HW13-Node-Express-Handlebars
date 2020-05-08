c3d5e6fa92c89bbf6a627bc4399b78dae6db4cb5
APPLE BANANA <tedbelanoff@gmail.com>
2020-05-08 16:03:58 -0400

    HW13

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = []
 
 if (process.env.CLEARDB_DATABASE_URL)
 //Heroku connection
-{connection=mysql.createConnection(process.env.CLEARDB_DATABASE_URL)}
+{connection=mysql.createConnection(process.env.CLEARDB_DATABASE_URL, connectionLimit=30)}
 else {
 connection = mysql.createConnection({
 //Local connection
