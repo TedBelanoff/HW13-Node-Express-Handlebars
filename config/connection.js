eccd0ba0099cb948b1dd226ef97fef6bb4ff1376
APPLE BANANA <tedbelanoff@gmail.com>
2020-05-08 10:53:24 -0400

    Remote Config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Welcome10",
-  database: "burger_db"
+//Local
+//  host: "localhost",
+//  port: 3306,
+//  user: "root",
+//  password: "Welcome10",
+//  database: "burger_db"
+
+//Heroku
+ HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b872385f33a19f",
+  PASSWORD: "cb214664",
+  DB: "heroku_9c4800ffcdea580"
 });
 
 // Make connection.
